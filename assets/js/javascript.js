5e7f5da5d8e35534a440b2134cdc9e37ef53dd06
JohanABlomqvist <Therionpoe@gmail.com>
2023-03-20 20:25:05 +0000

    add javascript to rotate ships and gameboard added

---
+++ assets/js/javascript.js
@@ -0,0 +1,29 @@
+/* To rotate ships with the flip button */
+const gamesBoardContainer = document.querySelector('#gamesboard-container')
+const optionContainer = document.querySelector('.option-container')
+const flipButton = document.querySelector('#flip-button')
+
+let angle = 0
+function flip() {
+const optionShips = Array.from(optionContainer.children)
+    if (angle === 0) {
+        angle = 90
+    } else {
+        angle = 0
+    }
+    optionShips.forEach(optionShip => optionShip.style.transform = `rotate(${angle}deg)`)
+}
+
+/* Game board */
+const width = 10
+
+function createboard() {
+    const gameBoardContainer = document.createElement('div')
+    gameBoardContainer.classList.add('game-board')
+    gameBoardContainer.style.backgroundColor = 'pink'
+
+    gamesBoardContainer.append(gameBoardContainer)
+}
+createboard()
+
+flipButton.addEventListener('click', flip)
