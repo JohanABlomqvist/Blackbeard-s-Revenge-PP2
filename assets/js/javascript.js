cb626a513083bb1448b0628622a199057ca2e5aa
JohanABlomqvist <Therionpoe@gmail.com>
2023-03-21 08:50:56 +0000

    add gameboard grids through javascript

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -17,13 +17,22 @@ const optionShips = Array.from(optionContainer.children)
 /* Game board */
 const width = 10
 
-function createboard() {
+function createBoard(color, user) {
     const gameBoardContainer = document.createElement('div')
     gameBoardContainer.classList.add('game-board')
-    gameBoardContainer.style.backgroundColor = 'pink'
+    gameBoardContainer.style.backgroundColor = color
+    gameBoardContainer.id = user
+
+    for (let i = 0; i < width * width; i++) {
+        const block = document.createElement('div')
+        block.classList.add('block')
+        block.id = i
+        gameBoardContainer.append(block)
+    }
 
     gamesBoardContainer.append(gameBoardContainer)
 }
-createboard()
+createBoard('yellow', 'player')
+createBoard('pink', 'computer')
 
 flipButton.addEventListener('click', flip)
