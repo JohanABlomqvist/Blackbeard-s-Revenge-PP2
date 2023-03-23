162c3d1175fbe4f63c0a297d705522fa4f89b515
JohanABlomqvist <Therionpoe@gmail.com>
2023-03-23 16:53:36 +0000

    trying to add mobile functionality

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -117,6 +117,36 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
   rotateButton.addEventListener('click', rotate)
+  
+// https://codepen.io/deepakkadarivel/pen/LrGEdL is where I found the code
+  window.onload = function() {
+    // find the element that you want to drag.
+    var box = document.getElementById('grid-display');
+    
+    /* listen to the touchMove event,
+    every time it fires, grab the location
+    of touch and assign it to box */
+    
+    box.addEventListener('touchmove', function(e) {
+      // grab the location of touch
+      var touchLocation = e.targetTouches[0];
+      
+      // assign box new coordinates based on the touch.
+      box.style.left = touchLocation.pageX + 'px';
+      box.style.top = touchLocation.pageY + 'px';
+    })
+    
+    /* record the position of the touch
+    when released using touchend event.
+    This will be the drop position. */
+    
+    box.addEventListener('touchend', function(e) {
+      // current box position.
+      var x = parseInt(grid-display.style.left);
+      var y = parseInt(grid-display.style.top);
+    })
+    
+  }
 
   /* move around user ship */
   ships.forEach(ship => ship.addEventListener('dragstart', dragStart))
